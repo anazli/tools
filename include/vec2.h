449f104287e04137eeff7215f676df5352d60525
anazli <andreas221b@gmail.com>
2021-07-04 18:07:42 +0200

    Vec2: template, not only double

--- include/vec2.h
+++ include/vec2.h
@@ -2,6 +2,7 @@
 #define VEC2_H
 
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <random>
 
@@ -13,42 +14,43 @@
  *
  */
 
+template <class T>
 class Vec2 {
  public:
   //! Constructor with default arguments. If no parameters
   //! are provided, a vector (0,0) is created. Otherwise
   //! a vector (x,y,z) is created.
-  Vec2(double p1 = 0., double p2 = 0.) : m_x{p1}, m_y{p2} {}
+  Vec2(const T& p1 = 0., const T& p2 = 0.) : m_x{p1}, m_y{p2} {}
 
   //! Copy Constructor. Creates a vector that is a copy
   //! of an existing vector provided as argument.
-  Vec2(const Vec2& v) : m_x{v.x()}, m_y{v.y()} {}
+  Vec2(const Vec2<T>& v) : m_x{v.x()}, m_y{v.y()} {}
 
   // Getters
-  double x() const { return m_x; }  //!< returns the x component.
-  double y() const { return m_y; }  //!< returns the y component.
+  T x() const { return m_x; }  //!< returns the x component.
+  T y() const { return m_y; }  //!< returns the y component.
 
   // Setters
-  void setX(const double& p) { m_x = p; }  //!< sets the x component.
-  void setY(const double& p) { m_y = p; }  //!< sets the y component.
+  void setX(const T& p) { m_x = p; }  //!< sets the x component.
+  void setY(const T& p) { m_y = p; }  //!< sets the y component.
 
-  void setXY(const double& num) {
+  void setXY(const T& num) {
     setX(num);
     setY(num);
   }  //!< sets the two components to a specific value.
 
-  void setXY(const double& p1, const double& p2) {
+  void setXY(const T& p1, const T& p2) {
     setX(p1);
     setY(p2);
   }  //!< Every component gets a unique value.
 
-  double operator[](int i) const {
+  T operator[](int i) const {
     assert(i >= 0 && i <= 1);
     if (i == 0) return m_x;
     return m_y;
   }
 
-  double& operator[](int i) {
+  T& operator[](int i) {
     assert(i >= 0 && i <= 1);
     if (i == 0) return m_x;
     return m_y;
@@ -60,97 +62,262 @@ class Vec2 {
     return *this;
   }
 
-  Vec2 operator+() const {
-    return Vec2(m_x, m_y);
+  Vec2<T> operator+() const {
+    return Vec2<T>(m_x, m_y);
   };  //!< Overloaded unary (+) operator. Returns the same vector.
-  Vec2 operator-() const {
-    return Vec2(-m_x, -m_y);
+  Vec2<T> operator-() const {
+    return Vec2<T>(-m_x, -m_y);
   }  //!< Overloaded unary (-) operator. Returns the opposite vector.
 
   // Prefix
-  Vec2& operator++();  //!< Overloaded prefix (++) operator. Returns the vector
-                       //!< and increments its components by one.
-  Vec2& operator--();  //!< Overloaded prefix (--) operator. Returns the vector
-                       //!< and decrements its components by one.
+  Vec2<T>& operator++();  //!< Overloaded prefix (++) operator. Returns the
+                          //!< vector and increments its components by one.
+  Vec2<T>& operator--();  //!< Overloaded prefix (--) operator. Returns the
+                          //!< vector and decrements its components by one.
   // Postfix
-  Vec2 operator++(int);  //!< Overloaded postfix (++) operator. Increments the
-                         //!< vectors components by one and returns it.
-  Vec2 operator--(int);  //!< Overloaded postfix (--) operator. Decrements the
-                         //!< vectors components by one and returns it.
+  Vec2<T> operator++(int);  //!< Overloaded postfix (++) operator. Increments
+                            //!< the vectors components by one and returns it.
+  Vec2<T> operator--(int);  //!< Overloaded postfix (--) operator. Decrements
+                            //!< the vectors components by one and returns it.
 
-  Vec2& operator+=(
-      const double& num);  //!< Overloaded unary (+=) operator. Increments the
-                           //!< vector's components by a value num.
-  Vec2& operator+=(const Vec2& v);
-  Vec2& operator-=(
-      const double& num);  //!< Overloaded unary (-=) operator. Decrements the
-                           //!< vector's components by a value num.
-  Vec2& operator-=(const Vec2& v);
-  Vec2& operator*=(
-      const double& num);  //!< Overloaded unary (*=) operator. Multiplies the
-                           //!< vector's components by a value num.
+  Vec2<T>& operator+=(
+      const T& num);  //!< Overloaded unary (+=) operator. Increments the
+                      //!< vector's components by a value num.
+  Vec2<T>& operator+=(const Vec2<T>& v);
+  Vec2<T>& operator-=(
+      const T& num);  //!< Overloaded unary (-=) operator. Decrements the
+                      //!< vector's components by a value num.
+  Vec2<T>& operator-=(const Vec2<T>& v);
+  Vec2<T>& operator*=(
+      const T& num);  //!< Overloaded unary (*=) operator. Multiplies the
+                      //!< vector's components by a value num.
 
-  Vec2& normalize();      //!< Makes the vector a unit vector of length one.
-  double length() const;  //!< Returns the length of the vector.
+  Vec2<T>& normalize();  //!< Makes the vector a unit vector of length one.
+  double length() const  //!< Returns the length of the vector.
+  {
+    return sqrt(x() * x() + y() * y());
+  }
 
  private:
-  double m_x;
-  double m_y;
+  T m_x;
+  T m_y;
 };
 
-Vec2 operator+(
-    const Vec2&,
-    const Vec2&);  //!< Overloaded binary (+) operator. Addition of two vectors.
-Vec2 operator+(const Vec2&,
-               const double&);  //!< Overloaded binary (+) operator. Addition of
-                                //!< a vector and a number.
-Vec2 operator+(const double&,
-               const Vec2&);  //!< Overloaded binary (+) operator. Addition of a
-                              //!< vector and a number.
+//--------------------------------------------
+// Overloaded Member operators
+//--------------------------------------------
 
-Vec2 operator-(const Vec2&, const Vec2&);  //!< Overloaded binary (-) operator.
-                                           //!< Subtraction of two vectors.
-Vec2 operator-(const Vec2&,
-               const double&);  //!< Overloaded binary (-) operator. Subtraction
-                                //!< of a vector and a number.
-Vec2 operator-(const double&,
-               const Vec2&);  //!< Overloaded binary (-) operator. Subtraction
-                              //!< of a vector and a number.
+template <typename T>
+Vec2<T>& Vec2<T>::operator++() {
+  *this += 1;  // use of += member operator
+  return *this;
+}
 
-Vec2 operator*(const Vec2&, const Vec2&);  //!< Overloaded binary (*) operator.
-                                           //!< Multiplication of two vectors.
-Vec2 operator*(const Vec2&,
-               const double&);  //!< Overloaded binary (*) operator.
-                                //!< Multiplication of a vector and a number.
-Vec2 operator*(const double&,
-               const Vec2&);  //!< Overloaded binary (*) operator.
-                              //!< Multiplication of a vector and a number.
+template <typename T>
+Vec2<T>& Vec2<T>::operator--() {
+  *this -= 1;
+  return *this;
+}
 
-Vec2 operator/(const Vec2&,
-               const Vec2&);  //!< Overloaded binary (/) operator. Division of
-                              //!< two vectors component by component.
-Vec2 operator/(const Vec2&,
-               const double&);  //!< Overloaded binary (/) operator. Division of
-                                //!< a vector's components by a number.
+template <typename T>
+Vec2<T> Vec2<T>::operator++(int) {
+  Vec2 temp(m_x, m_y);
+  ++(*this);  // use of prefix ++ operator
+  return temp;
+}
 
-bool operator==(const Vec2&,
-                const Vec2&);  //!< Overloaded binary (==) operator. Checks if
-                               //!< two vectors are identical.
-bool operator!=(const Vec2&,
-                const Vec2&);  //!< Overloaded binary (!=) operator. Checks if
-                               //!< two vectors are not identical.
+template <typename T>
+Vec2<T> Vec2<T>::operator--(int) {
+  Vec2 temp(m_x, m_y);
+  --(*this);
+  return temp;
+}
 
-std::istream& operator>>(std::istream& in,
-                         Vec2& v);  //!< Overloaded binary (>>) operator. Reads
-                                    //!< a vector's components from a stream.
-std::ostream& operator<<(
-    std::ostream&, const Vec2&);  //!< Overloaded binary (<<) operator. Prints a
-                                  //!< vector's components to a stream.
+template <typename T>
+Vec2<T>& Vec2<T>::operator+=(const T& num) {
+  *this = (*this) + num;  // use of binary + operator
+  return *this;
+}
 
-double dot(const Vec2&, const Vec2&);  //!< The dot product of two vectors.
-                                       //!< Returns a number of type double.
+template <typename T>
+Vec2<T>& Vec2<T>::operator+=(const Vec2& v) {
+  *this = (*this) + v;
+  return *this;
+}
 
-Vec2 getUnitVectorOf(const Vec2&);  //!< Gets the unit vector of a vector
-                                    //!< without changing the provided vector.
+template <typename T>
+Vec2<T>& Vec2<T>::operator-=(const T& num) {
+  *this = (*this) - num;
+  return *this;
+}
+
+template <typename T>
+Vec2<T>& Vec2<T>::operator-=(const Vec2<T>& v) {
+  *this = (*this) - v;
+  return *this;
+}
+
+template <typename T>
+Vec2<T>& Vec2<T>::operator*=(const T& num) {
+  *this = (*this) * num;
+  return *this;
+}
+
+template <typename T>
+Vec2<T>& Vec2<T>::normalize() {
+  double norm = this->length();
+  *this = (*this) / (norm + 1.E-30);
+  return *this;
+}
+
+//--------------------------------------------
+// Overloaded I/O operators (input, output)
+//--------------------------------------------
+
+template <typename T>
+inline std::istream& operator>>(std::istream& in, Vec2<T>& v) {
+  T x, y;
+  in >> x >> y;
+  v.setXY(x, y);
+  return in;
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream& out, const Vec2<T>& v) {
+  out << "(" << v.x() << "," << v.y() << ")";
+  return out;
+}
+
+//--------------------------------------------
+// Overloaded operators as normal functions
+// Binary operator (+, -, *)
+//--------------------------------------------
+
+template <typename T>
+Vec2<T> operator+(const Vec2<T>& v1,
+                  const Vec2<T>& v2)  //!< Overloaded binary (+) operator.
+                                      //!< Addition of two vectors.
+{
+  return Vec2<T>(v1.x() + v2.x(), v1.y() + v2.y());
+}
+template <typename T>
+Vec2<T> operator+(const Vec2<T>& v,
+                  const T& num)  //!< Overloaded binary (+) operator. Addition
+                                 //!< of !< a vector and a number.
+{
+  return Vec2<T>(v.x() + num, v.y() + num);
+}
+template <typename T>
+Vec2<T> operator+(const T& num,
+                  const Vec2<T>& v)  //!< Overloaded binary (+) operator.
+                                     //!< Addition of a !< vector and a number.
+{
+  return v + num;
+}
+
+template <typename T>
+Vec2<T> operator-(const Vec2<T>& v1,
+                  const Vec2<T>& v2)  //!< Overloaded binary (-) operator. !<
+                                      //!< Subtraction of two vectors.
+{
+  return Vec2<T>(v1.x() - v2.x(), v1.y() - v2.y());
+}
+
+template <typename T>
+Vec2<T> operator-(const Vec2<T>& v,
+                  const T& num)  //!< Overloaded binary (-) operator.
+                                 //!< Subtraction !< of a vector and a number.
+{
+  return Vec2<T>(v.x() - num, v.y() - num);
+}
+template <typename T>
+Vec2<T> operator-(
+    const T& num,
+    const Vec2<T>& v)  //!< Overloaded binary (-) operator. Subtraction !< of a
+                       //!< vector and a number.
+{
+  return v - num;
+}
+
+template <typename T>
+Vec2<T> operator*(const Vec2<T>& v1,
+                  const Vec2<T>& v2)  //!< Overloaded binary (*) operator. !<
+                                      //!< Multiplication of two vectors.
+{
+  return Vec2<T>(v1.x() * v2.x(), v1.y() * v2.y());
+}
+template <typename T>
+Vec2<T> operator*(const Vec2<T>& v,
+                  const T& num)  //!< Overloaded binary (*) operator. !<
+                                 //!< Multiplication of a vector and a number.
+{
+  return Vec2<T>(v.x() * num, v.y() * num);
+}
+template <typename T>
+Vec2<T> operator*(
+    const T& num,
+    const Vec2<T>& v)  //!< Overloaded binary (*) operator. !< Multiplication of
+                       //!< a vector and a number.
+{
+  return v * num;
+}
+
+template <typename T>
+Vec2<T> operator/(
+    const Vec2<T>& v1,
+    const Vec2<T>& v2)  //!< Overloaded binary (/) operator. Division of !< two
+                        //!< vectors component by component.
+{
+  if (v2.x() == (T)0 || v2.y() == (T)0) throw "Division by zero!";
+  return Vec2<T>(v1.x() / v2.x(), v1.y() / v2.y());
+}
+template <typename T>
+Vec2<T> operator/(const Vec2<T>& v,
+                  const T& num)  //!< Overloaded binary (/) operator. Division
+                                 //!< of !< a vector's components by a number.
+{
+  T e = 1.E-30;
+  return Vec2<T>(v.x() / (num + e), v.y() / (num + e));
+}
+
+template <typename T>
+bool operator==(const Vec2<T>& v1,
+                const Vec2<T>& v2)  //!< Overloaded binary (==) operator. Checks
+                                    //!< if !< two vectors are identical.
+{
+  return (v1.x() == v2.x() && v1.y() == v2.y());
+}
+template <typename T>
+bool operator!=(const Vec2<T>& v1,
+                const Vec2<T>& v2)  //!< Overloaded binary (!=) operator. Checks
+                                    //!< if !< two vectors are not identical.
+{
+  return !(v1 == v2);
+}
+
+//--------------------------------------------
+// General methods
+//--------------------------------------------
+
+template <typename T>
+T dot(const Vec2<T>& v1,
+      const Vec2<T>& v2)  //!< The dot product of two vectors. !< Returns a
+                          //!< number of type double.
+{
+  Vec2<T> v = v1 * v2;
+  return v.x() + v.y();
+}
+
+template <typename T>
+Vec2<T> getUnitVectorOf(
+    const Vec2<T>& v)  //!< Gets the unit vector of a vector !< without changing
+                       //!< the provided vector.
+{
+  return v / (v.length() + 1.E-30);
+}
+
+typedef Vec2<double> Vec2d;
+typedef Vec2<float> Vec2f;
+typedef Vec2<int> Vec2i;
 
 #endif  // VEC2_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 #include "../include/vec2.h"
-#include "../include/vec3.h"
-#include "../include/vec4.h"
 
 using namespace std;
 
 int main() {
-  Vec3 x_unit(1., 0., 0.);
+  Vec2d v(1.5, 2.5);
+  cout << v << endl;
+  cout << v.normalize() << endl;
+
+  /*Vec3 x_unit(1., 0., 0.);
   Vec3 y_unit(0., 1., 0.);
   Vec3 z_unit = cross(x_unit, y_unit);
-  cout << z_unit << endl;
+  cout << z_unit << endl;*/
   return 0;
 }
