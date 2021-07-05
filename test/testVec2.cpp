f5fc110ef8d0917c292702a001e373ee4dff1f3e
anazli <andreas221b@gmail.com>
2021-07-05 21:24:03 +0200

    Vec2: a few more Tests

--- test/testVec2.cpp
+++ test/testVec2.cpp
@@ -1,17 +1,79 @@
-#include "gmock/gmock.h"
 #include "../include/vec2.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 using namespace testing;
 
 class Vector2 : public Test {
-    public:
-        Vec2<double> v;
+ public:
+  Vec2<double> v;
 };
 
+TEST_F(Vector2, CreatesVector) {
+  v = Vec2<double>(0., 0.);
+  ASSERT_DOUBLE_EQ(v.x(), 0.);
+  ASSERT_DOUBLE_EQ(v.y(), 0.);
+}
 
-TEST_F(Vector2, CreatesVector)
-{
-    v = Vec2<double>(0., 0.);
-    ASSERT_DOUBLE_EQ(v.x(), 0.);
-    ASSERT_DOUBLE_EQ(v.y(), 0.);
+TEST_F(Vector2, AssertsOutOfBounds) {
+  v = Vec2<double>(1., 2.);
+  ASSERT_DEATH(v[2], "");
+  ASSERT_DEATH(v[-1], "");
+}
+
+TEST_F(Vector2, SetsCoords) {
+  v.setX(5.);
+  v[1] = 4.;
+  ASSERT_DOUBLE_EQ(v.x(), 5.);
+  ASSERT_DOUBLE_EQ(v[1], 4.);
+}
+
+TEST_F(Vector2, SetXY) {
+  v.setXY(-1.24, -5.54);
+  ASSERT_DOUBLE_EQ(v.x(), -1.24);
+  ASSERT_DOUBLE_EQ(v.y(), -5.54);
+
+  v.setXY(6.68);
+  ASSERT_DOUBLE_EQ(v.x(), 6.68);
+  ASSERT_DOUBLE_EQ(v.y(), 6.68);
+}
+
+TEST_F(Vector2, ChangesSign) {
+  v.setXY(-1.55);
+  v = -v;
+  ASSERT_DOUBLE_EQ(v[0], 1.55);
+  ASSERT_DOUBLE_EQ(v[1], 1.55);
+}
+
+TEST_F(Vector2, AddsVectorOrNumber) {
+  v = Vec2<double>(0., 0.);
+  v += 4.46;
+  ASSERT_DOUBLE_EQ(v.x(), 4.46);
+  ASSERT_DOUBLE_EQ(v.y(), 4.46);
+
+  v += Vec2<double>(4., 6.);
+  ASSERT_DOUBLE_EQ(v.x(), 8.46);
+  ASSERT_DOUBLE_EQ(v.y(), 10.46);
+}
+
+TEST_F(Vector2, SubtractsVectorOrNumber) {
+  v = Vec2<double>(0., 0.);
+  v -= 4.46;
+  ASSERT_DOUBLE_EQ(v.x(), -4.46);
+  ASSERT_DOUBLE_EQ(v.y(), -4.46);
+
+  v -= Vec2<double>(4., 6.);
+  ASSERT_DOUBLE_EQ(v.x(), -8.46);
+  ASSERT_DOUBLE_EQ(v.y(), -10.46);
+}
+
+TEST_F(Vector2, GetsLenghtOfVector) {
+  v = Vec2<double>(0., 0.);
+  ASSERT_DOUBLE_EQ(v.length(), 0.);
+  v = Vec2<double>(1., 1.);
+  ASSERT_DOUBLE_EQ(v.length(), sqrt(2.));
+  v = Vec2<double>(3., 3.);
+  ASSERT_DOUBLE_EQ(v.length(), sqrt(18.));
+  v = Vec2<double>(-5., -5.);
+  ASSERT_DOUBLE_EQ(v.length(), sqrt(50.));
 }
