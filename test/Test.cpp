58d2ce005f4cf1623dd4eda1b62baf702b91de59
anazli <andreas221b@gmail.com>
2021-07-11 17:33:18 +0200

    Mat3: more Tests

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "../include/tools.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -537,7 +539,7 @@ TEST_F(Matrix2, MultipliesWithNumber) {
 
 TEST_F(Matrix2, GetsTheDeterminant) {
   m = Mat2<double>(Vec2<double>(5.36, 2.28), Vec2<double>(-1.5, 85.));
-  ASSERT_DOUBLE_EQ(m.Determinant(), 459.02);
+  ASSERT_DOUBLE_EQ(m.determinant(), 459.02);
 }
 
 TEST_F(Matrix2, AddsTwoMatrices) {
@@ -556,4 +558,178 @@ TEST_F(Matrix2, MultipliesTwoMatrices) {
   ASSERT_DOUBLE_EQ(m[0][1], -1.104);
   ASSERT_DOUBLE_EQ(m[1][0], 13.5);
   ASSERT_DOUBLE_EQ(m[1][1], -8.7);
+}
+
+//--------------------------------------------
+//     MAT3
+//--------------------------------------------
+
+class Matrix3 : public Test {
+ public:
+  Mat3<double> m;
+};
+
+TEST_F(Matrix3, CreatesMatrix) {
+  Vec3<double> v1(0., 0., 0.);
+  Vec3<double> v2(1., -1., 1.);
+  Vec3<double> v3(5., -9., 5.);
+
+  m = Mat3<double>(v1, v2, v3);
+  ASSERT_DOUBLE_EQ(m[0][0], v1[0]);
+  ASSERT_DOUBLE_EQ(m[0][1], v1[1]);
+  ASSERT_DOUBLE_EQ(m[0][2], v1[2]);
+
+  ASSERT_DOUBLE_EQ(m[1][0], v2[0]);
+  ASSERT_DOUBLE_EQ(m[1][1], v2[1]);
+  ASSERT_DOUBLE_EQ(m[1][2], v2[2]);
+
+  ASSERT_DOUBLE_EQ(m[2][0], v3[0]);
+  ASSERT_DOUBLE_EQ(m[2][1], v3[1]);
+  ASSERT_DOUBLE_EQ(m[2][2], v3[2]);
+
+  m = Mat3<double>(3.14);
+  ASSERT_DOUBLE_EQ(m[0][0], 3.14);
+  ASSERT_DOUBLE_EQ(m[0][1], 3.14);
+  ASSERT_DOUBLE_EQ(m[0][2], 3.14);
+
+  ASSERT_DOUBLE_EQ(m[1][0], 3.14);
+  ASSERT_DOUBLE_EQ(m[1][1], 3.14);
+  ASSERT_DOUBLE_EQ(m[1][2], 3.14);
+
+  ASSERT_DOUBLE_EQ(m[2][0], 3.14);
+  ASSERT_DOUBLE_EQ(m[2][1], 3.14);
+  ASSERT_DOUBLE_EQ(m[2][2], 3.14);
+}
+
+TEST_F(Matrix3, AddsAmatrixOrNumber) {
+  m = Mat3<double>(0.);
+  m += Mat3<double>(-1.);
+  ASSERT_DOUBLE_EQ(m[0][0], -1.);
+  ASSERT_DOUBLE_EQ(m[0][1], -1.);
+  ASSERT_DOUBLE_EQ(m[0][2], -1.);
+
+  ASSERT_DOUBLE_EQ(m[1][0], -1.);
+  ASSERT_DOUBLE_EQ(m[1][1], -1.);
+  ASSERT_DOUBLE_EQ(m[1][2], -1.);
+
+  ASSERT_DOUBLE_EQ(m[2][0], -1.);
+  ASSERT_DOUBLE_EQ(m[2][1], -1.);
+  ASSERT_DOUBLE_EQ(m[2][2], -1.);
+  m += 5.05;
+  ASSERT_DOUBLE_EQ(m[0][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[0][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[0][2], 4.05);
+
+  ASSERT_DOUBLE_EQ(m[1][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[1][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[1][2], 4.05);
+
+  ASSERT_DOUBLE_EQ(m[2][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[2][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[2][2], 4.05);
+}
+
+TEST_F(Matrix3, SubtractsAmatrixOrNumber) {
+  m = Mat3<double>(0.);
+  m -= Mat3<double>(1.);
+  ASSERT_DOUBLE_EQ(m[0][0], -1.);
+  ASSERT_DOUBLE_EQ(m[0][1], -1.);
+  ASSERT_DOUBLE_EQ(m[0][2], -1.);
+  ASSERT_DOUBLE_EQ(m[1][0], -1.);
+  ASSERT_DOUBLE_EQ(m[1][1], -1.);
+  ASSERT_DOUBLE_EQ(m[1][2], -1.);
+  ASSERT_DOUBLE_EQ(m[2][0], -1.);
+  ASSERT_DOUBLE_EQ(m[2][1], -1.);
+  ASSERT_DOUBLE_EQ(m[2][2], -1.);
+  m -= -5.05;
+  ASSERT_DOUBLE_EQ(m[0][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[0][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[0][2], 4.05);
+  ASSERT_DOUBLE_EQ(m[1][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[1][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[1][2], 4.05);
+  ASSERT_DOUBLE_EQ(m[2][0], 4.05);
+  ASSERT_DOUBLE_EQ(m[2][1], 4.05);
+  ASSERT_DOUBLE_EQ(m[2][2], 4.05);
+}
+
+TEST_F(Matrix3, MultipliesWithNumber) {
+  m = Mat3<double>(6.28);
+  m *= 0.;
+  ASSERT_DOUBLE_EQ(m[0][0], 0.);
+  ASSERT_DOUBLE_EQ(m[0][1], 0.);
+  ASSERT_DOUBLE_EQ(m[0][2], 0.);
+  ASSERT_DOUBLE_EQ(m[1][0], 0.);
+  ASSERT_DOUBLE_EQ(m[1][1], 0.);
+  ASSERT_DOUBLE_EQ(m[1][2], 0.);
+  ASSERT_DOUBLE_EQ(m[2][0], 0.);
+  ASSERT_DOUBLE_EQ(m[2][1], 0.);
+  ASSERT_DOUBLE_EQ(m[2][2], 0.);
+}
+
+TEST_F(Matrix3, GetsTheDeterminant) {
+  m = Mat3<double>(Vec3<double>(5.36, 2.28, 0.93), Vec3<double>(-1.5, 85., 0.),
+                   Vec3<double>(45.8, -50., 0.14));
+  ASSERT_DOUBLE_EQ(m.determinant(), -3486.4772);
+}
+
+TEST_F(Matrix3, AddsTwoMatrices) {
+  m = Mat3<double>(Vec3<double>(1.36, 1.28, 1.), Vec3<double>(1.5, 0., -1.),
+                   Vec3<double>(6.3, 0.5, 0.8));
+  m = m + Mat3<double>(Vec3<double>(9., -5.8, 0.),
+                       Vec3<double>(-6.5, 5.3, -1.5),
+                       Vec3<double>(-5., 0.4, 90.));
+  ASSERT_DOUBLE_EQ(m[0][0], 10.36);
+  ASSERT_DOUBLE_EQ(m[0][1], -4.52);
+  ASSERT_DOUBLE_EQ(m[0][2], 1.);
+
+  ASSERT_DOUBLE_EQ(m[1][0], -5.);
+  ASSERT_DOUBLE_EQ(m[1][1], 5.3);
+  ASSERT_DOUBLE_EQ(m[1][2], -2.5);
+
+  ASSERT_DOUBLE_EQ(m[2][0], 1.3);
+  ASSERT_DOUBLE_EQ(m[2][1], 0.9);
+  ASSERT_DOUBLE_EQ(m[2][2], 90.8);
+}
+
+TEST_F(Matrix3, MultipliesTwoMatrices) {
+  m = Mat3<double>(Vec3<double>(1.36, 1.28, 0.85), Vec3<double>(1.5, 0., -6.58),
+                   Vec3<double>(4.5, 0., -3.));
+  Mat3<double> m2(
+      Vec3<double>(9., -5.8, -4.5), Vec3<double>(-6.5, 5.3, 20.),
+      Vec3<double>(8.6, 33.,
+                   -5.));  // With an anonymous object, it doesn't pass
+  m = m * m2;
+
+  ASSERT_DOUBLE_EQ(m[0][0], 11.23);
+  ASSERT_DOUBLE_EQ(m[0][1], 26.946);
+  ASSERT_DOUBLE_EQ(m[0][2], 15.23);
+
+  ASSERT_DOUBLE_EQ(m[1][0], -43.088);
+  ASSERT_DOUBLE_EQ(m[1][1], -225.84);
+  ASSERT_DOUBLE_EQ(m[1][2], 26.15);
+
+  ASSERT_DOUBLE_EQ(m[2][0], 14.7);
+  ASSERT_DOUBLE_EQ(m[2][1], -125.1);
+  ASSERT_DOUBLE_EQ(m[2][2], -5.25);
+}
+
+TEST_F(Matrix3, GetTransposeOfMatrix)
+{
+  m = Mat3<double>(Vec3<double>(1.36, 1.28, 0.85), Vec3<double>(1.5, 0., -6.58),
+                   Vec3<double>(4.5, 0., -3.));
+  
+  Mat3<double> mt = m.transpose();
+  ASSERT_DOUBLE_EQ(mt[0][0], 1.36);
+  ASSERT_DOUBLE_EQ(mt[0][1], 1.5);
+  ASSERT_DOUBLE_EQ(mt[0][2], 4.5);
+
+  ASSERT_DOUBLE_EQ(mt[1][0], 1.28);
+  ASSERT_DOUBLE_EQ(mt[1][1], 0.);
+  ASSERT_DOUBLE_EQ(mt[1][2], 0.);
+
+  ASSERT_DOUBLE_EQ(mt[2][0], 0.85);
+  ASSERT_DOUBLE_EQ(mt[2][1], -6.58);
+  ASSERT_DOUBLE_EQ(mt[2][2], -3.);
+
 }
